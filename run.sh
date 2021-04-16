0d2917e0ebed9435ba49a92bad12b8af271b52d6
knemeth <nemkrisz11@gmail.com>
2021-04-16 15:01:18 +0200

    Server minor progress

--- run.sh
+++ run.sh
@@ -2,7 +2,6 @@ export PYTHONPATH="$PYTHONPATH:$PWD/secureFTP/"
 
 python3 ./secureFTP/netsim/network.py -p "./secureFTP/network/" -a "ABC" --clean &
 python3 ./secureFTP/server/server.py -p "./secureFTP/network/" -a "A" &
-jobs
 python3 ./secureFTP/app.py -p "./secureFTP/network/"
 kill %2
 kill %1
--- secureFTP/server/server.py
+++ secureFTP/server/server.py
@@ -1,10 +1,12 @@
 from secureFTP.netsim.communicator import Communicator
-from cryptography.hazmat.primitives import ciphers, hashes, padding
+from secureFTP.protocol.header import *
+from cryptography.hazmat.primitives import ciphers, hashes, padding, serialization
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives.serialization import Encoding, PublicFormat
 import os
 import sys
 import getopt
+import secrets
 
 
 class ServerCaller(type):
@@ -16,6 +18,9 @@ class ServerCaller(type):
 
 
 class FTPServer(Communicator, metaclass=ServerCaller):
+
+    lt_server_private_key = None
+    lt_server_public_key = None
 
     """
     dict of dicts, the keys are network identifiers
@@ -34,8 +39,14 @@ class FTPServer(Communicator, metaclass=ServerCaller):
         super().__init__(address, net_path)
 
         # Generate server long-term keypair
+        self.lt_server_private_key = ec.generate_private_key(ec.SECP521R1())
+
+        # Save public key
+
+        # Serialize, encrypt and save private key
 
         # Create server certificate
+
 
     def serve(self):
         while True:
@@ -49,21 +60,34 @@ class FTPServer(Communicator, metaclass=ServerCaller):
 
             if msg_src in self.active_sessions.keys():
                 # Existing connection, look up sessionID
-
                 session = self.active_sessions[msg_src]
-                id = session["SessionID"]
+                session_id = session["SessionID"]
 
-                pass # TODO
+                pass  # TODO
 
             else:
                 # New session init
+                # Split message
+                header = msg_src[1:17]  # 16 bytes of header
+                if header != init_header:
+                    print("Header mismatch detected!")
+                    # TODO : error handling
+
+                client_proof = msg_src[17:49]  # 32 bytes of client proof
+                ecdh_client_public_key = serialization.load_der_public_key(msg_src[49:])
+
                 # Generate ephemeral ECDH keypair
                 ecdh_server_private_key = ec.generate_private_key(ec.SECP521R1())
                 ecdh_server_public_key = ecdh_server_private_key.public_key()
 
-                # Choose SessionID for the session
+                # Choose a non-colliding SessionID for the session
+                while True:
+                    session_id = secrets.token_hex(8)
+                    if not any(session_id in s["sessionID"] for s in self.active_sessions.values()):
+                        break
 
                 # Construct session key
+
 
                 # Save session parameters
 
